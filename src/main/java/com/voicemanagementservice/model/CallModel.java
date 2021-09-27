4e932547f546bb61968e39ecad024f8153f3be30
Kawsikan <kawsikan.kumaralingam@techventuras.com>
2021-09-27 11:59:01 +0530

    changes

--- src/main/java/com/voicemanagementservice/model/CallModel.java
+++ src/main/java/com/voicemanagementservice/model/CallModel.java
@@ -3,18 +3,14 @@ package com.voicemanagementservice.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Getter
 @Setter
-@Entity
+//@Entity
 public class CallModel {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String mobileNumber;
     private String callSID;

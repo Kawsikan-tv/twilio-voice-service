1a9f14f53c17fbb51c41c25d7fd539a2c3a3f1ba
Kawsikan <kawsikan.kumaralingam@techventuras.com>
2021-09-27 11:57:23 +0530

    changes

--- src/main/java/com/voicemanagementservice/repository/CallRepository.java
+++ src/main/java/com/voicemanagementservice/repository/CallRepository.java
@@ -1,7 +1,6 @@
 package com.voicemanagementservice.repository;
 
 import com.voicemanagementservice.model.CallModel;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CallRepository extends JpaRepository<CallModel, Long> {
+public interface CallRepository {
 }
